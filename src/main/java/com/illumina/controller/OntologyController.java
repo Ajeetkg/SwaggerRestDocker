2d02fa89ffa625a44238092af4c76a5d9dcbd6ab
Ajeet Gupta <agupta2@illumina.com>
2015-11-02 12:45:51 -0800

    Added logging functionality

--- src/main/java/com/illumina/controller/OntologyController.java
+++ src/main/java/com/illumina/controller/OntologyController.java
@@ -11,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import com.illumina.service.OntologyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -19,12 +21,14 @@ import java.util.List;
 @Api(value="Ontology", description = "Ontology API")
 public class OntologyController {
 
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @Autowired
     OntologyService ontologyService;
 
     @RequestMapping(value="/ont/ontology", method=RequestMethod.POST)
     @ApiOperation(httpMethod = "POST", value="Add Ontology to domain")
     public ResponseEntity<OntologyNode> addOntologyByDomainId(@RequestBody OntologyNode ontologyNode) {
+        logger.info("Post ontology node: "+ontologyNode);
         HttpHeaders httpHeaders = new HttpHeaders();
         ontologyNode = ontologyService.addOntologyByDomainId(ontologyNode);
         return new ResponseEntity<OntologyNode>(ontologyNode, httpHeaders, HttpStatus.CREATED);
@@ -33,6 +37,7 @@ public class OntologyController {
     @RequestMapping(value="/ont/ontology", method=RequestMethod.GET)
     @ApiOperation(httpMethod = "GET", value="Get the available Ontology")
     public ResponseEntity<List<OntologyNode>> getOntologyByDomainId(@RequestParam(value = "id", defaultValue = "1") String domainId) {
+        logger.info("Retrieve ontology node for: "+domainId);
         List<OntologyNode> conceptList = ontologyService.getOntologyByDomainId(domainId);
         return new ResponseEntity<List<OntologyNode>>(conceptList, HttpStatus.OK);
     }
