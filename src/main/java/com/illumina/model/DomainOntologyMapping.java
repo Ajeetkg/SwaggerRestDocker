931a186ed0f342345764e54b48032ee0ac76d0c1
Ajeet Gupta <agupta2@illumina.com>
2015-11-02 16:41:04 -0800

    Datasource added

--- src/main/java/com/illumina/model/DomainOntologyMapping.java
+++ src/main/java/com/illumina/model/DomainOntologyMapping.java
@@ -1,12 +1,10 @@
 package com.illumina.model;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "DOMAIN_ONTOLOGY_MAPPING")
 public class DomainOntologyMapping {
 
     @Id
--- src/main/java/com/illumina/model/Ontology.java
+++ src/main/java/com/illumina/model/Ontology.java
@@ -1,9 +1,6 @@
 package com.illumina.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 
 /**
@@ -11,6 +8,7 @@ import javax.persistence.Id;
  */
 
 @Entity
+@Table(name = "ONTOLOGY")
 public class Ontology {
 
     @Id
