ac6d76c28188de8495ad502e3639161a80b723f0
Ajeet Gupta <agupta2@illumina.com>
2015-11-04 10:52:20 -0800

    Added the exception handling - still need to refine

--- src/main/java/com/illumina/controller/OntologyController.java
+++ src/main/java/com/illumina/controller/OntologyController.java
@@ -3,16 +3,16 @@ package com.illumina.controller;
 
 import com.illumina.domain.OntologyRequest;
 import com.illumina.domain.OntologyResult;
+import com.illumina.service.OntologyService;
 import com.illumina.util.Constants;
 import com.wordnik.swagger.annotations.Api;
 import com.wordnik.swagger.annotations.ApiOperation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.illumina.service.OntologyService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 
 @RestController
@@ -26,16 +26,26 @@ public class OntologyController {
     @RequestMapping(value = "/ont/ontology/{domainid}", method=RequestMethod.GET)
     @ApiOperation(httpMethod = Constants.GET, value="Get the mapped ontology for domainId")
     public ResponseEntity<OntologyResult> getMappedOntologyForDomain(@PathVariable("domainid")String domainId){
-        logger.info("getMappedOntologyForDomain.Mapped Ontology for domain:"+domainId);
-        OntologyResult ontologyResult = ontologyService.getMappedOntologyForDomain(domainId);
-        return new ResponseEntity<>(ontologyResult, HttpStatus.OK);
+        try {
+            logger.info("getMappedOntologyForDomain.Mapped Ontology for domain:" + domainId);
+            OntologyResult ontologyResult = ontologyService.getMappedOntologyForDomain(domainId);
+            return new ResponseEntity<>(ontologyResult, HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
     }
 
     @RequestMapping(value = "/ont/ontology/{domainid}", method=RequestMethod.PUT)
     @ApiOperation(httpMethod = Constants.PUT, value="Replace mapped Ontology for a domainId")
     public ResponseEntity<OntologyResult> updateOntologyForDomain(@PathVariable("domainid")String domainId, @RequestBody OntologyRequest request){
-        logger.info("updateOntologyForDomain.Mapped Ontology for domain:"+request);
-        OntologyResult ontologyResult  = ontologyService.updateOntologyForDomain(domainId, request);
-        return new ResponseEntity<>(HttpStatus.OK);
+
+        try {
+            logger.info("updateOntologyForDomain.Mapped Ontology for domain:" + request);
+            OntologyResult ontologyResult  = ontologyService.updateOntologyForDomain(domainId, request);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
